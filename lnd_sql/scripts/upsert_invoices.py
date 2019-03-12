e272ae32346211192a83c33af4bf4141149f25a8
Pierre Rochard <pierre@rochard.org>
2019-03-12 14:04:06 -0500

    make upsert method static

--- lnd_sql/scripts/upsert_invoices.py
+++ lnd_sql/scripts/upsert_invoices.py
@@ -16,23 +16,18 @@ from lnd_sql.models.lnd.etl.etl_invoices import ETLInvoices
 
 
 class UpsertInvoices(object):
-    rpc: Client
+    _rpc: Client
 
     def __init__(self,
                  tls_cert_path: str = None,
                  macaroon_path: str = None,
-                 lnd_network: str = 'mainnet',
                  lnd_grpc_host: str = '127.0.0.1',
                  lnd_grpc_port: str = '10009'):
-        self.rpc = Client(
-            tls_cert_path=tls_cert_path,
-            macaroon_path=macaroon_path,
-            network=lnd_network,
-            grpc_host=lnd_grpc_host,
-            grpc_port=lnd_grpc_port,
-        )
-
-        self.info: GetInfoResponse = self.rpc.get_info()
+        self._rpc = None
+        self.tls_cert_path = tls_cert_path
+        self.macaroon_path = macaroon_path
+        self.lnd_grpc_host = lnd_grpc_host
+        self.lnd_grpc_port = lnd_grpc_port
 
     @staticmethod
     def get_index_offset() -> int:
@@ -43,16 +38,31 @@ class UpsertInvoices(object):
             )
             return record
 
+    @property
+    def rpc(self) -> Client:
+        if self._rpc is None:
+            self._rpc = Client(
+                tls_cert_path=self.tls_cert_path,
+                macaroon_path=self.macaroon_path,
+                grpc_host=self.lnd_grpc_host,
+                grpc_port=self.lnd_grpc_port,
+            )
+        return self._rpc
+
     def upsert_all(self):
         invoices: ListInvoiceResponse = self.rpc.list_invoices(
             num_max_invoices=100000,
             index_offset=self.get_index_offset()
         )
-        self.upsert(invoice_list=list(invoices.invoices),
-                    last_index_offset=invoices.last_index_offset)
+        info: GetInfoResponse = self.rpc.get_info()
 
-    def upsert(self, single_invoice=None, invoice_list=None,
-               last_index_offset=None):
+        self.upsert(invoice_list=list(invoices.invoices),
+                    last_index_offset=invoices.last_index_offset,
+                    local_pubkey=info.identity_pubkey)
+
+    @staticmethod
+    def upsert(single_invoice=None, invoice_list=None,
+               last_index_offset=None, local_pubkey=None):
         if invoice_list is None:
             invoice_list = [single_invoice]
 
@@ -62,7 +72,7 @@ class UpsertInvoices(object):
         for invoice in invoice_list:
             invoice_dict = MessageToDict(invoice)
             invoice_dict['last_index_offset'] = last_index_offset
-            invoice_dict['local_pubkey'] = self.info.identity_pubkey
+            invoice_dict['local_pubkey'] = local_pubkey
             invoice_dict['r_preimage'] = invoice.r_preimage.hex()
             invoice_dict['r_hash'] = invoice.r_hash.hex()
             invoice_dict['description_hash'] = invoice.description_hash.hex()
